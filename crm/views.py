d06ca70a4d3773a0a548c3e0213e9ece072d57b8
Daniel Quintana <quintana.danielarmando@gmail.com>
2023-08-11 20:07:32 +0100

    create django project with authentication

---
+++ crm/views.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+def homepage(request):
+    return render(request, "crm/index.html")
+
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my-login")
+    
+    context = {"registerform": form}
+    return render(request, "crm/register.html", context=context)
+
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            user = authenticate(request=request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+    context = {"loginform": form}
+    return render(request, "crm/my-login.html", context=context)
+
+
+def user_logout(request):
+    auth.logout(request)
+
+    return redirect("")
+
+@login_required(login_url="my-login")
+def dashbpard(request):
+    return render(request, "crm/dashboard.html")
